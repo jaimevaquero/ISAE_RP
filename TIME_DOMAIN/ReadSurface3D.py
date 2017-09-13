3de929cc42d5554707d521af5a9f96306309c9e9
Jaime Vaquero <jaimevaquerog@gmail.com>
2017-09-13 15:19:17 +0200

    ReadSurface3D modified

--- TIME_DOMAIN/ReadSurface3D.py
+++ TIME_DOMAIN/ReadSurface3D.py
@@ -2,7 +2,7 @@
 #
 # Function for surface reading in the advanced time approach of the 
 # FW-H method.
-# Autor: Jaime Vaquero
+# Author: Jaime Vaquero
 # September 2017
 #
 # ****************************************************************************
@@ -13,4 +13,16 @@ def ReadSurface3D(x,y,z,variables,Cartesian=True):
         print(' ReadSurface3D: using cartesian coordinates')
     else: 
         print(' ReadSurface3D: using cylindrical coordinates')
-              
+   
+
+def SortData(variables,sortingVariable):
+    sortindex  = np.argsort(sortingVariable)
+    outputList = [] 
+    for x in variables:
+       x2 = np.zeros(length(x))
+       i = -1
+       for m in sortindex:
+           i     = i + 1
+           x2[i] = x[m] 
+       outputList.append(x) 
+    return outputList
